1d54b88d19646b7424c4ed56339985d87f864622
dev-ahmedali <ahmedali69325@gmail.com>
2022-09-23 23:16:43 +0600

    added news section of widgets

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -5,7 +5,7 @@ import {BellIcon, BookmarkIcon, ClipboardIcon, HashtagIcon, InboxIcon, UserIcon,
 
 export default function Sidebar() {
   return (
-    <div className='hidden sm:flex flex-col p-2 xl:items-start fixed h-full'>
+    <div className='hidden sm:flex flex-col p-2 xl:items-start fixed h-full xl:ml-24'>
         {/* logo */}
         <div className='hoverEffect p-0 hover:bg-red-100 xl:px-1'>
             <h3>Post Now</h3>
