148db6bcc5d2af782bdf518961bb1ca7d1bf8be1
dev-ahmedali <ahmedali69325@gmail.com>
2022-09-27 13:21:34 +0600

    add delete functionallity on post

--- components/Post.js
+++ components/Post.js
@@ -8,11 +8,18 @@ import {
   TrashIcon,
 } from '@heroicons/react/outline';
 import { HeartIcon as HeartIconFilled } from '@heroicons/react/solid';
-import { collection, deleteDoc, doc, onSnapshot, setDoc } from 'firebase/firestore';
+import {
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  setDoc,
+} from 'firebase/firestore';
 import Moment from 'react-moment';
-import { db } from '../firebase';
+import { db, storage } from '../firebase';
 import { signIn, useSession } from 'next-auth/react';
 import { useEffect, useState } from 'react';
+import { deleteObject, ref } from 'firebase/storage';
 
 export default function Post({ post }) {
   const { data: session } = useSession();
@@ -21,29 +28,37 @@ export default function Post({ post }) {
 
   useEffect(() => {
     const unsubscribe = onSnapshot(
-      collection(db, "posts", post.id, "likes"),
+      collection(db, 'posts', post.id, 'likes'),
       (snapshot) => setLikes(snapshot.docs)
     );
   }, [db]);
 
   useEffect(() => {
-    setHasLikes(likes.findIndex((like) => like.id === session?.user.uid) !== -1);
+    setHasLikes(
+      likes.findIndex((like) => like.id === session?.user.uid) !== -1
+    );
   }, [likes]);
 
   async function likePost() {
-    if(session) {
-
-      if(hasLiked) {
-        await deleteDoc(doc(db, "posts", post.id, "likes", session?.user.uid))
+    if (session) {
+      if (hasLiked) {
+        await deleteDoc(doc(db, 'posts', post.id, 'likes', session?.user.uid));
       } else {
         await setDoc(doc(db, 'posts', post.id, 'likes', session?.user.uid), {
           username: session.user.username,
         });
-      } 
+      }
     } else {
-      signIn()
+      signIn();
     }
-   
+  }
+
+  async function deletePost() {
+    if (window.confirm('Are you sure you want to delete to this post?'))
+    {
+      deleteDoc(doc(db, 'posts', post.id));
+      deleteObject(ref(storage, `posts/${post.id}/image`));
+    } 
   }
   return (
     <div className="flex p-3 cursor-pointer border-b border-gray-200">
@@ -87,25 +102,31 @@ export default function Post({ post }) {
 
         <div className="flex justify-between text-gray-500 p-2">
           <ChatIcon className="h-9 hoverEffect w-9 p-2 hover:text-pink-500 hover:bg-pink-100" />
-          <TrashIcon className="h-9 hoverEffect w-9 p-2 hover:text-red-700 hover:bg-red-100" />
-          <div className='flex items-center'>
-
-          {hasLiked ? (
-            <HeartIconFilled
-              onClick={likePost}
-              className="h-9 hoverEffect w-9 p-2 text-red-500 hover:bg-red-100"
-            />
-          ) : (
-            <HeartIcon
-              onClick={likePost}
-              className="h-9 hoverEffect w-9 p-2 hover:text-red-500 hover:bg-red-100"
+          {session?.user.uid === post?.data().id && (
+            <TrashIcon
+              onClick={deletePost}
+              className="h-9 hoverEffect w-9 p-2 hover:text-red-700 hover:bg-red-100"
             />
           )}
-          {
-            likes.length > 0 && (
-              <span className={`${hasLiked && "text-red-600"}text-sm select-none`}>{likes.length}</span>
-            )
-          }
+          <div className="flex items-center">
+            {hasLiked ? (
+              <HeartIconFilled
+                onClick={likePost}
+                className="h-9 hoverEffect w-9 p-2 text-red-500 hover:bg-red-100"
+              />
+            ) : (
+              <HeartIcon
+                onClick={likePost}
+                className="h-9 hoverEffect w-9 p-2 hover:text-red-500 hover:bg-red-100"
+              />
+            )}
+            {likes.length > 0 && (
+              <span
+                className={`${hasLiked && 'text-red-600'}text-sm select-none`}
+              >
+                {likes.length}
+              </span>
+            )}
           </div>
 
           <ShareIcon className="h-9 hoverEffect w-9 p-2 hover:text-pink-500 hover:bg-pink-100" />
